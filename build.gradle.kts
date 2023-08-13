31477a80c39b1d9d073341919818a8cdff8d984b
Tiago Couto <t.couto@carapay.com>
2023-08-13 10:31:30 +0200

    Setting up Gradle: - properties - version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,9 +27,6 @@ plugins {
     kotlin("plugin.spring") version "1.8.22"
 }
 
-group = "io.github.tiagodocouto"
-version = "0.0.1-SNAPSHOT"
-
 java {
     sourceCompatibility = JavaVersion.VERSION_17
 }
